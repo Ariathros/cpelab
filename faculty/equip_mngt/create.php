f2d1135cbdfe437184761cb2f43a9ebc3ab6e650
KMB <110606372+mIn0uT@users.noreply.github.com>
2023-03-07 08:21:05 +0800

    Faculty Equipment CRUD

---
+++ faculty/equip_mngt/create.php
@@ -0,0 +1,100 @@
+<?php
+    include "../../connections.php";
+    include '../sessions.php';
+
+    if(isset($_POST['submit'])) {
+        $equip_code = $_POST['equip_code'];
+        $equip_name = $_POST['equip_name'];
+        $category = $_POST['category'];
+        $total = $_POST['total'];
+		$available = $_POST['available'];
+
+        // SQL query to add new record
+        $sql = "INSERT INTO `equipments`(`id`, `equip_code`, `equip_name`, `category`, `total`, `available`) 
+		VALUES (NULL,'$equip_code','$equip_name','$category','$total', '$available')";
+
+        $result = mysqli_query($conn, $sql);
+
+        if($result) {
+            header("Location: equip-mngt.php?msg=New Equipment Created Succesfully");
+        }
+        else {
+            echo "Failed: " . mysqli_error($conn); 
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+        <title>Create Equipment - CPE Lab Room and Equipment Management System</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: #4D0000; color: white;">
+            Equipment Management
+        </nav>
+
+        <div class="container">
+            <div class="text-center mb-4">
+                <h3>Add New Equipmnet</h3>
+                <p class="text-muted">Complete the form below to add new equipment</p>
+            </div>
+
+            <div class="container d-flex justify-content-center">
+                <form action="" method="post" style="width:50vw; min-width:300px;">
+					<!-- Equip Code -->
+                    <div class="mb-3">
+                        <label class="form-label">Code:</label>
+                        <input type="text" class="form-control" name="equip_code" placeholder="pjt001">
+                    </div>
+
+					<!-- Equip Name -->
+                    <div class="mb-3">
+                        <label class="form-label">Name:</label>
+                        <input type="text" class="form-control" name="equip_name" placeholder="Projector">
+                    </div>
+
+					<!-- Category -->
+                    <div class="mb-3">
+						<label class="form-label">Category</label>
+						<select name="category" class="form-control">
+							<option value="">--Select Option--</option>
+							<option value="laptop">Laptop</option>
+							<option value="projector">Projector</option>
+							<option value="electronics">Electronics</option>
+						</select>
+                    </div>
+
+					<!-- Total -->
+                    <div class="mb-3">
+                        <label class="form-label">Total:</label>
+                        <input type="number" max="50" min="0" class="form-control" name="total" placeholder="max: 50, min: 0">
+                    </div>
+
+					<!-- Available -->
+                    <div class="mb-3">
+                        <label class="form-label">Available:</label>
+                        <input type="number" max="50" min="0"class="form-control" name="available" placeholder="max: 50, min: 0">
+                    </div>
+    
+
+                    <div class="mb-5">
+                        <button type="submit" class="btn btn-success" name="submit">Save</button>
+                        <a href="equip-mngt.php" class="btn btn-danger">Cancel</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!-- Bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+        </script>
+    </body>
+</html>
--- faculty/equip_mngt/equip-mngt.php
+++ faculty/equip_mngt/equip-mngt.php
@@ -1,61 +1,87 @@
 <?php
-	include '../connections.php';	
-	include 'sessions.php';
+	//include '../connections.php';	
+	//include 'sessions.php';
 ?>
 
-<HTML>
-	<HEAD>
-		<TITLE>Equipment Management - CPE Lab Room and Equipment Management System</TITLE>
-	</HEAD>
-	
-	<BODY>
-		<DIV>
-			<?php include 'sidebar.php'; ?>
-		</DIV>
-		
-		<DIV>
-			<H1>Equipment Reservations</H1>
-		</DIV>
-		
-		<DIV>
-			<A HREF="add-eq.php">Add Equipment</A>
-		</DIV>
-		
-		<DIV>
-			<TABLE>
-				<TR>
-					<TH SCOPE="COL">Equipment Code</TH>
-					<TH SCOPE="COL">Equipment Name</TH>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- Bootstrap -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" 
+		integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" 
+		integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" 
+		crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+		<title>Faculty Equipment Management - CPE Lab Room and Equipment Management System</title>
+	</head>
+	<body>
+		<?php
+			include "../sidebar.php";
+		?>
+
+		<nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: #4D0000; color: white;">
+            Equipment Management
+        </nav>
+
+		<div class="container">
+			<?php
+				if(isset($_GET['msg'])) {
+					$msg = $_GET['msg'];
+					echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+					'.$msg.'
+					<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+				  </div>';
+				}
+			?>
+
+			<a href="create.php" class="btn btn-dark mb-3">Add New</a>
+
+			<table class="table table-hover text-center">
+				<thead class="table-dark">
+					<tr>
+					<TH SCOPE="COL">ID</TH>
+					<TH SCOPE="COL">Code</TH>
+					<TH SCOPE="COL">Name</TH>
 					<TH SCOPE="COL">Category</TH>
 					<TH SCOPE="COL">Total</TH>
 					<TH SCOPE="COL">Available</TH>
-					<TH SCOPE="COL">Actions</TH>
-				</TR>
-				
-				<?php
-					$sql = "SELECT * FROM equipments";
-					$result = $conn->query($sql);
+					<TH SCOPE="COL">Action</TH>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						include '../../connections.php';
+						include '../sessions.php';
 
-					if ($result->num_rows > 0) {
-						// output data of each row
-						while($row = $result->fetch_assoc()) {
-							echo "<TR>
-								<TD>" . $row["equip_code"]. "</TD>
-								<TD>" . $row["equip_name"]. "</TD>
-								<TD>" . $row["category"]. "</TD>
-								<TD>" . $row["total"]. "</TD>
-								<TD>" . $row["available"]. "</TD>
-								<TD>
-									<A HREF='equip-edit.php?id=".$row["id"]."&action=edit'>Edit</A>
-									<A HREF='equip-edit.php?id=".$row["id"]."&action=delete'>Delete</A>
-								</TD>
-							</TR>";
+						$sql = "SELECT * FROM equipments";
+						$result = mysqli_query($conn, $sql);
+						// This will get all data from our database
+						while ($row = mysqli_fetch_assoc($result)) {
+							?>
+							<tr>
+								<td><?php echo $row['id']?></td>
+								<td><?php echo $row['equip_code']?></td>
+								<td><?php echo $row['equip_name']?></td>
+								<td><?php echo $row['category']?></td>
+								<td><?php echo $row['total']?></td>
+								<td><?php echo $row['available']?></td>
+								<td><a href="edit.php?id=<?php echo $row['id']?>" class="link-dark"><i class="fa-solid fa-pen-to-square me-3"></i></a> <!--From fontawesome plugin-->
+									<a href="delete.php?id=<?php echo $row['id']?>" class="link-dark"><i class="fa-solid fa-trash fs-5"></i></a></td>
+							</tr>
+							<?php
 						}
-					} else {
-						echo "<TR><TD>0 results</TD></TR>";
-					}
-				?>
-			</TABLE>
-		</DIV>
-	</BODY>
-</HTML>
+					?>
+				</tbody>
+			</table>
+		</div>
+		<!-- Bootstrap -->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" 
+		integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+		</script>
+	</body>
+</html>
