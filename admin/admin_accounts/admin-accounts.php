16fa6baf1c6d9fcad2ef1dc537d50273b8f9be4c
KMB <110606372+mIn0uT@users.noreply.github.com>
2023-03-10 08:44:26 +0800

    fixed html

--- admin/admin_accounts/admin-accounts.php
+++ admin/admin_accounts/admin-accounts.php
@@ -20,47 +20,26 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 		<script src="https://code.jquery.com/jquery-4.0.0.min.js" ></script>
 
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="../../assets/css/style.css"></link>
 	</head>
 	<body>
-		<?php
-			include "../sidebar.php";
-		?>
+		<div class="row">
+			<div class="col-3 px-2">
+				<?php
+					include "../sidebar.php";
+				?>
+			</div>
+			<div class="col-9 px-0" >
+				<DIV style="padding-top:24px; padding-left:24px; padding-right:24px;">
+					<H1 class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: #4D0000; color: white;">
+						Account Management
+					</H1>
+				</DIV>
 
-		<nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: #4D0000; color: white;">
-            Account Management
-        </nav>
-
-		<div class="container">
-			<?php
-				if(isset($_GET['msg'])) {
-					$msg = $_GET['msg'];
-					echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
-					'.$msg.'
-					<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-				  </div>';
-				}
-			?>
-
-			<a href="create.php" class="btn btn-dark mb-3">Add New</a>
-
-			<table class="table table-hover text-center">
-				<thead class="table-dark">
-					<tr>
-					<th scope="col">ID</th>
-					<th scope="col">First Name</th>
-					<th scope="col">Last Name</th>
-					<th scope="col">Username</th>
-					<th scope="col">Id No.</th>
-					<th scope="col">Email</th>
-					<th scope="col">Password</th>
-					<th scope="col">User Type</th>
-					<th scope="col">Action</th>
-					</tr>
-				</thead>
-				<tbody>
+				<div class="container">
 					<?php
 						if(isset($_GET['msg'])) {
 							$msg = $_GET['msg'];
@@ -72,7 +51,6 @@
 					?>
 
 					<a href="create.php" class="btn btn-dark mb-3">Add New</a>
-
 					<table class="table table-hover text-center">
 						<thead class="table-dark">
 							<tr>
@@ -96,8 +74,7 @@
 								// Get and show all data from our database
 								$sql = "SELECT * FROM useraccounts";
 								$result = mysqli_query($conn, $sql);
-								while ($row = mysqli_fetch_assoc($result)) {
-									?>
+								while ($row = mysqli_fetch_assoc($result)) { ?>
 									<tr>
 										<td><?php echo $row['id']?></td>
 										<td><?php echo $row['firstname']?></td>
@@ -110,9 +87,7 @@
 										<td><a href="edit.php?id=<?php echo $row['id']?>" class="link-dark"><i class="fa-solid fa-pen-to-square me-3"></i></a> <!--From fontawesome plugin-->
 											<a href="delete.php?id=<?php echo $row['id']?>" class="link-dark"><i class="fa-solid fa-trash fs-5"></i></a></td>
 									</tr>
-									<?php
-								}
-							?>
+								<?php } ?>
 						</tbody>
 					</table>
 				</div>
