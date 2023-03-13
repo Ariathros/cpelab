d40fac738aa570cfbf26abefeb643fdcddf02b62
Vince Jeremy Ladion <55903157+vinceedesu@users.noreply.github.com>
2023-03-13 04:54:19 -0300

    sidebar kuno

--- faculty/room_mngt/room-mngt.php
+++ faculty/room_mngt/room-mngt.php
@@ -6,16 +6,20 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<!-- Bootstrap -->
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" 
-		integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-		<!-- Font Awesome -->
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" 
-		integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" 
-		crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+		<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="../../assets/js/ajaxWork.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+		<script src="https://code.jquery.com/jquery-4.0.0.min.js" ></script>
+
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="../../assets/css/style.css"></link>
 
 		<title>Faculty Room Management - CPE Lab Room and Equipment Management System</title>
 	</head>
--- faculty/sidebar.php
+++ faculty/sidebar.php
@@ -1,9 +1,45 @@
 
-	Welcome <?php echo $_SESSION['name'];?>!<BR>
-	<A HREF='/cpelab/faculty/faculty-dashboard.php'>Dashboard</A>
-	<A HREF='/cpelab/faculty/room/faculty-rooms.php'>Rooms</A>
-	<A HREF='/cpelab/faculty/room_mngt/room-mngt.php'>Room Management</A>
-	<A HREF='/cpelab/faculty/equip/faculty-equip.php'>Equipments</A>
-	<A HREF='/cpelab/faculty/equip_mngt/equip-mngt.php'>Equipment Management</A>
-	<A HREF='/cpelab/logout.php'>Log Out</A>
+<div class="sidebar"  style="color:white;">
+    <div class="side-header">
+        <img src="../../assets/images/pup logo.png" width="120px" height="120px">
+    </div>
+    <div class="menu">
+        <div>
+            <a class="btn" HREF='../dashboard/faculty-dashboard.php'>
+                <div class="menu_icon"><i class='fas fa-layer-group'></i></div>
+                <div class="menu_text">Dashboard</div>
+            </a>
+        </div>
+        <div>
+            <a class="btn" HREF='../room/faculty-rooms.php'>
+                <div class="menu_icon"><i class='fas fa-door-open'></i></div>
+                <div class="menu_text">Room</div>
+            </a>
+        </div>
+        <div>
+            <a class="btn" HREF='../room_mngt/room-mngt.php'>
+                <div class="menu_icon"><i class='fas fa-solid fa-list-check'></i></div>
+                <div class="menu_text">Room Management</div>
+            </a>
+        </div>
+        <div>
+            <a class="btn" HREF='../equip/faculty-equip.php'>
+                <div class="menu_icon"><i class='fas fa-screwdriver-wrench'></i></div>
+                <div class="menu_text">Equipment</div>
+            </a>
+        </div>
+		<div>
+			<a class="btn" HREF='../equip_mngt/equip-mngt.php'>
+				<div class="menu_icon"><i class='fa-solid fa-toolbox'></i></div>
+				<div class="menu_text">Equipment Management</div>
+			</a>
+		</div>
+    </div>
+    <div class="logout">
+        <a href="../../logout.php" class="btn">
+            <div class="menu_icon"><i class='fas fa-sign-out-alt'></i></div>
+            <div class="menu_text">Logout</div>
+        </a>
+    </div>
+</div>
 
