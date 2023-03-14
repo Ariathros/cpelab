5f28ec36c4cee782579f972d9810a2243087f99e
KMB <110606372+mIn0uT@users.noreply.github.com>
2023-03-14 11:20:00 +0800

    equipment pagination

--- faculty/equip/faculty-equip.php
+++ faculty/equip/faculty-equip.php
@@ -1,6 +1,35 @@
 <?php
 	include '../../connections.php';	
 	include '../sessions.php';
+
+	// get page number
+	if (isset($_GET['page_no']) && $_GET['page_no'] !== "") {
+		$page_no = $_GET['page_no'];
+	}else {
+		$page_no = 1;
+	}
+	// These are all required for pagination implementation
+	// count of records to display per page
+	$record_per_page = 10;
+	// page offset for LIMIT query
+	$offset = ($page_no -1) * $record_per_page;
+	// get previous page
+	$previous_page = $page_no - 1;
+	// get nxt page
+	$nextpage = $page_no + 1;
+	// get the total count of records
+	$record_count = mysqli_query($conn, "SELECT COUNT(*) AS total_records FROM eq_man") or die(mysqli_error($conn));
+	// total records
+	$records = mysqli_fetch_array($record_count);
+	// store total records to a variable
+	$total_records = $records['total_records'];
+	// get total pages
+	$total_pages = ceil($total_records / $record_per_page);
+
+	// sql query
+	$sql = "SELECT * FROM room_man LIMIT $offset , $record_per_page";
+	// result
+	$result = $conn->query($sql);
 ?>
 
 <HTML>
@@ -117,6 +146,32 @@
 					?>
 					</tbody>
 				</TABLE>
+				<!-- Pagination -->
+				<nav aria-label="Page navigation example">
+						<ul class="pagination">
+						<!-- Previous -->
+						<li class="page-item"><a class="page-link <?= ($page_no <= 1) ? 'disabled' : ''; ?>"
+						<?= ($page_no > 1) ? 'href=?page_no=' . $previous_page : ''; ?>>Previous</a></li>
+						<!-- Page Numbers -->
+							<?php for($counter = 1; $counter <= $total_pages; $counter++) { ?>
+								<?php if($page_no != $counter) { ?>
+									<li class="page-item"><a class="page-link" href="?page_no=<?=
+									$counter; ?>"><?= $counter; ?></a></li>
+								<?php } else { ?>
+									<li class="page-item"><a class="page-link active"><?= $counter; ?>
+									</a></li>
+								<?php } ?>
+							<?php } ?>
+
+							<!-- Next -->
+							<li class="page-item"><a class="page-link <?= ($page_no >= $total_pages) ? 'disabled' : ''; ?>"
+							<?= ($page_no < $total_pages) ? 'href=?page_no=' . $nextpage : ''; ?>>Next</a></li>
+						</ul>
+					</nav>
+					<!-- Page navigation -->
+					<div class="p-10">
+						<strong>Page <?= $page_no; ?> of <?= $total_pages; ?></strong>
+					</div>
 			</DIV>
 			</div>
 		</div>
