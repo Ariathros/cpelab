5459b7eb99e6a56a12111d877dffd9d4728a7f01
Jan Carlo Abalos <73739927+Ariathros@users.noreply.github.com>
2023-03-15 16:34:48 +0800

    page fix 2

--- faculty/room/faculty-rooms.php
+++ faculty/room/faculty-rooms.php
@@ -20,7 +20,7 @@
 	// get nxt page
 	$nextpage = $page_no + 1;
 	// get the total count of records
-	$record_count = mysqli_query($conn, "SELECT COUNT(*) AS total_records FROM room_man") or die(mysqli_error($conn));
+	$record_count = mysqli_query($conn, "SELECT COUNT(*) AS total_records FROM room_man WHERE status='Approved'") or die(mysqli_error($conn));
 	// total records
 	$records = mysqli_fetch_array($record_count);
 	// store total records to a variable
@@ -29,7 +29,7 @@
 	$total_pages = ceil($total_records / $record_per_page);
 
 	// sql query
-	$sql = "SELECT * FROM room_man LIMIT $offset , $record_per_page";
+	$sql = "SELECT * FROM room_man WHERE status='Approved' LIMIT $offset , $record_per_page";
 	// result
 	$result = $conn->query($sql);
 ?>
