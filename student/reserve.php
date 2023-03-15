86e0fe62cc2631bd53d1b46a4cd52837569ed51c
KMB <110606372+mIn0uT@users.noreply.github.com>
2023-03-15 11:03:40 +0800

    placed SQL INSERT at the top
    Wala nang lumalabas na header error pero hindi naman gumagana yun header redirection

--- student/reserve.php
+++ student/reserve.php
@@ -1,6 +1,25 @@
 <?php
 	include '../connections.php';	
 	include 'sessions.php';
+	// Variables for validation
+	$timeErr = '';
+	$hasErr = false;
+	// Executed if no error from validation
+	if($hasErr) {
+		if (isset($_POST['bReserve'])){
+			// Get the values from the html form
+			$time_start = htmlentities($_POST['time_start']);
+			$time_end = htmlentities($_POST['time_end']);
+			$reason = htmlentities($_POST['reason']);
+			$date = htmlentities($_POST['date']);
+			// Insert to SQL
+			$sql = "INSERT INTO room_man (room_no, room_type, borrower, reason, date, time_start, time_end, status)
+				VALUES ('$room_no', '$room_type', '".$_SESSION['username']."', '$reason', '$date', '$time_start', '$time_end', 'Pending')";
+			$conn->query($sql);
+			header('Location: student-index.php');
+		}
+	}
+			
 ?>
 
 <HTML>
@@ -31,7 +50,6 @@
 				?>
 			</DIV>
 			<div class="col-9 px-0">
-				
 				<div class="reservation_form container allContent-section" style="background-color: #F5F5F5">
 					<div class="reservation_title">
 						<u >Reservation Form</u>
@@ -48,13 +66,8 @@
 						$seat_count = $row['seat_count'];
 						$room_status = $row['room_status'];
 						
-						$timeErr = '';
-						$hasErr = false;
-
 						// condition to check if start time is greater than end time
 						if($_SERVER['REQUEST_METHOD'] == 'POST') {
-							
-
 							$sql = "SELECT * FROM room_man WHERE room_no='$room_no' AND status='Approved'";
 							$result = $conn->query($sql);
 							$reserved=0;
@@ -81,7 +94,7 @@
 									}	
 								}	
 							}
-
+							// End time must be greater than start time
 							if($start1 > $end1) {
 								$timeErr = "Start time must not greater than end time.";
 							}elseif($reserved==1){
@@ -92,6 +105,7 @@
 							}
 						}
 					?>
+					<!-- Reserve form -->
 					<FORM METHOD="POST">
 						<div class="input-group mb-3">
 							<div class="input-title">Room No.:</div>
@@ -131,7 +145,7 @@
 						<INPUT class="btn" NAME="bReserve" TYPE='SUBMIT'>
 					</FORM>
 				</div>
-				<?php
+				<!-- <?php
 					if($hasErr) {
 						if (isset($_POST['bReserve'])){
 
@@ -147,7 +161,7 @@
 							header('Location: student-index.php');
 						}
 					}
-				?>
+				?> -->
 			</div>
 		</div>
 		<script src="https://unpkg.com/@popperjs/core@2"></script>
